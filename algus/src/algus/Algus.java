f607bcc460901596d94b87761f191ca6fade3443
Rauno Piibor <31948944+raunopiibor123@users.noreply.github.com>
2018-02-01 09:25:53 +0200

    Väljundi muutus

--- algus/src/algus/Algus.java
+++ algus/src/algus/Algus.java
@@ -16,7 +16,7 @@ public class Algus {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       System.out.println("Hello, Web");
+       System.out.println("Hello, Web!");
     }
     
 }
