6f3d574ac0ca9883a592962934eb88ace7fb1db8
if17 <if17@A406-3.INFORMAATIKA>
2018-02-01 10:21:00 +0200

    Uue projekti loomine

--- algus/src/algus/Algus.java
+++ algus/src/algus/Algus.java
@@ -10,10 +10,23 @@ package algus;
  * @author if17
  */
 public class Algus {
+    
+    /**
+     * Alamprogramm toa soojuse hindamiseks vastavalt temperatuurile.
+     * Vähemasti kolm taset
+     * /
+  
+    /**
+     * @param temperatuur 
+     */
+    
 
+    
     public static void ilmaKommentaar(int temperatuur){
-       if(temperatuur>0){System.out.println("sulab");}
-       else{System.out.println("Jäätab");}
+       if(temperatuur>=21){System.out.println("Soe on");}
+            if(temperatuur==20){System.out.println("Täpselt paras");}
+                if(temperatuur<20){System.out.println("Külmaks läheb");}
+
     }
    
     /**
@@ -22,7 +35,7 @@ public class Algus {
      */
     public static void main(String[] args) {
        System.out.println("Hello, Web!");
-       ilmaKommentaar(3);
+       ilmaKommentaar(101);
     }
     
 }
