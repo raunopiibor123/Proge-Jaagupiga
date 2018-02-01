7a70f8086cdb39c592c33e0a215642f0fe5dd290
if17 <if17@A406-3.INFORMAATIKA>
2018-02-01 09:33:06 +0200

    Uue projekti loomine

--- algus/src/algus/Algus.java
+++ algus/src/algus/Algus.java
@@ -11,12 +11,19 @@ package algus;
  */
 public class Algus {
 
+    public static void ilmaKommentaar(){
+       int temperatuur=-2;
+       if(temperatuur>0){System.out.println("sulab");}
+       else{System.out.println("Jäätab");}
+    }
+   
     /**
      * Programmi töö alguskoht. Siit alustatakse käivitamisel
      * @param args the command line arguments
      */
     public static void main(String[] args) {
        System.out.println("Hello, Web!");
+       ilmaKommentaar();
     }
     
 }
